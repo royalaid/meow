be0f0c1ef8f45c0b8df8aac7292ca3f8267b1592
Pablo <pablo@hashingsystems.com>
2023-12-27 17:56:28 -0600

    chore(beefyvault): niceties

--- solidity/contracts/BeefyVaultWithdrawal.sol
+++ solidity/contracts/BeefyVaultWithdrawal.sol
@@ -1,14 +1,14 @@
 pragma solidity 0.8.20;
 
 interface IERC20 {
-  function approve(address spender, uint256 amount) external returns (bool);
-  function transfer(address recipient, uint256 amount) external returns (bool);
-  function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
-  function balanceOf(address account) external view returns (uint256);
+  function approve(address _spender, uint256 _amount) external returns (bool success);
+  function transfer(address _recipient, uint256 _amount) external returns (bool success);
+  function transferFrom(address _sender, address _recipient, uint256 _amount) external returns (bool success);
+  function balanceOf(address _account) external view returns (uint256 balance);
 }
 
 interface IBeefy {
-  function getPricePerFullShare() external view returns (uint256);
+  function getPricePerFullShare() external view returns (uint256 pricePerFullShare);
 }
 
 contract BeefyVaultWithdrawal {
@@ -40,19 +40,19 @@ contract BeefyVaultWithdrawal {
   bool public paused;
 
   // Events
-  event Deposited(address indexed user, uint256 amount);
-  event Withdrawn(address indexed user, uint256 amount);
+  event Deposited(address indexed _user, uint256 _amount);
+  event Withdrawn(address indexed _user, uint256 _amount);
   event FeesUpdated(uint256 newDepositFee, uint256 newWithdrawalFee);
   event MinimumFeesUpdated(uint256 newMinimumDepositFee, uint256 newMinimumWithdrawalFee);
   event OwnerUpdated(address newOwner);
-  event MAIRemoved(address indexed user, uint256 amount);
-  event FeesWithdrawn(address indexed owner, uint256 feesEarned);
-  event PauseEvent(address account, bool paused);
-  event WithdrawalCancelled(address indexed user, uint256 amount);
-  event ScheduledWithdrawal(address indexed user, uint256 amount);
-  event WithdrawalScheduled(address indexed user, uint256 amount);
-  event MaxDepositUpdated(uint256 maxDeposit);
-  event MaxWithdrawUpdated(uint256 maxWithdraw);
+  event MAIRemoved(address indexed _user, uint256 _amount);
+  event FeesWithdrawn(address indexed _owner, uint256 feesEarned);
+  event PauseEvent(address _account, bool _paused);
+  event WithdrawalCancelled(address indexed _user, uint256 _amount);
+  event ScheduledWithdrawal(address indexed _user, uint256 _amount);
+  event WithdrawalScheduled(address indexed _user, uint256 _amount);
+  event MaxDepositUpdated(uint256 _maxDeposit);
+  event MaxWithdrawUpdated(uint256 _maxWithdraw);
 
   // target 0x9c4ec768c28520b50860ea7a15bd7213a9ff58bf
   constructor(address _gem, address _underlying, uint256 _depositFee, uint256 _withdrawalFee, address _target) {
@@ -81,17 +81,19 @@ contract BeefyVaultWithdrawal {
   }
 
   // user deposits shares, withdraws stable
-  function deposit(uint256 amount) external pausable {
-    require(amount >= minimumDepositFee && amount <= maxDeposit, 'Invalid amount');
+  function deposit(uint256 _amount) external pausable {
+    require(_amount >= minimumDepositFee && _amount <= maxDeposit, 'Invalid amount');
 
-    uint256 fee = calculateDepositFee(amount);
+    uint256 fee = calculateDepositFee(_amount);
 
-    require(amount > fee, 'Invalid amount');
+    require(_amount > fee, 'Invalid amount');
 
-    uint256 amountAfterFee = amount - fee;
+    uint256 amountAfterFee = _amount - fee;
     uint256 amountOfUnderlying = IBeefy(gem).getPricePerFullShare() * amountAfterFee / 1e18;
 
-    IERC20(gem).transferFrom(msg.sender, address(this), amount);
+    require(IERC20(MAI_ADDRESS).balanceOf(address(this)) >= amountOfUnderlying, 'Insufficient MAI balance');
+
+    IERC20(gem).transferFrom(msg.sender, address(this), _amount);
     totalStableLiquidity += amountOfUnderlying;
     accumulatedFees += fee;
 
@@ -100,29 +102,29 @@ contract BeefyVaultWithdrawal {
     emit Deposited(msg.sender, amountAfterFee);
   }
 
-  function scheduleWithdraw(uint256 amount) external pausable {
-    require(amount >= minimumWithdrawalFee && amount <= maxWithdraw, 'Invalid amount');
+  function scheduleWithdraw(uint256 _amount) external pausable {
+    require(_amount >= minimumWithdrawalFee && _amount <= maxWithdraw, 'Invalid amount');
     require(scheduledWithdrawalAmount[msg.sender] == 0, 'Withdrawal already scheduled');
 
-    IERC20(MAI_ADDRESS).transferFrom(msg.sender, address(this), amount);
-    scheduledWithdrawalAmount[msg.sender] = amount;
+    IERC20(MAI_ADDRESS).transferFrom(msg.sender, address(this), _amount);
+    scheduledWithdrawalAmount[msg.sender] = _amount;
     withdrawalEpoch[msg.sender] = getCurrentEpoch();
 
-    emit ScheduledWithdrawal(msg.sender, amount);
+    emit ScheduledWithdrawal(msg.sender, _amount);
   }
 
   function executeWithdrawal() external pausable {
     require(isWithinWithdrawalPeriod(withdrawalEpoch[msg.sender]), 'Not within withdrawal period');
 
-    uint256 amount = scheduledWithdrawalAmount[msg.sender];
-    require(amount <= totalStableLiquidity, 'Invalid amount');
+    uint256 _amount = scheduledWithdrawalAmount[msg.sender];
+    require(_amount <= totalStableLiquidity, 'Invalid amount');
 
-    uint256 amountOfShares = amount * 1e18 / IBeefy(gem).getPricePerFullShare();
+    uint256 amountOfShares = _amount * 1e18 / IBeefy(gem).getPricePerFullShare();
     uint256 fee = calculateWithdrawalFee(amountOfShares);
     require(amountOfShares > fee, 'Invalid amount after fee');
 
     uint256 amountOfSharesAfterFee = amountOfShares - fee;
-    totalStableLiquidity -= amount;
+    totalStableLiquidity -= _amount;
     accumulatedFees += fee;
 
     scheduledWithdrawalAmount[msg.sender] = 0;
@@ -137,31 +139,31 @@ contract BeefyVaultWithdrawal {
     require(scheduledWithdrawalAmount[msg.sender] > 0, 'No withdrawal scheduled');
     require(getCurrentEpoch() - withdrawalEpoch[msg.sender] < 3, 'Withdrawal already executable');
 
-    uint256 amount = scheduledWithdrawalAmount[msg.sender];
+    uint256 _amount = scheduledWithdrawalAmount[msg.sender];
     scheduledWithdrawalAmount[msg.sender] = 0;
     withdrawalEpoch[msg.sender] = 0;
 
-    IERC20(MAI_ADDRESS).transfer(msg.sender, amount);
-    emit WithdrawalCancelled(msg.sender, amount);
+    IERC20(MAI_ADDRESS).transfer(msg.sender, _amount);
+    emit WithdrawalCancelled(msg.sender, _amount);
   }
 
-  function getCurrentEpoch() public view returns (uint256) {
-    return block.timestamp / 1 days;
+  function getCurrentEpoch() public view returns (uint256 epoch) {
+    epoch = block.timestamp / 1 days;
   }
 
-  function isWithinWithdrawalPeriod(uint256 epoch) public view returns (bool) {
-    uint256 epochStartTime = epoch * 1 days;
-    return block.timestamp >= epochStartTime && block.timestamp <= epochStartTime + 12 hours;
+  function isWithinWithdrawalPeriod(uint256 _epoch) public view returns (bool withinPeriod) {
+    uint256 epochStartTime = _epoch * 1 days;
+    withinPeriod = block.timestamp >= epochStartTime && block.timestamp <= epochStartTime + 12 hours;
   }
 
-  function calculateDepositFee(uint256 amount) public view returns (uint256 fee) {
-    fee = amount * depositFee / 10_000;
-    fee < minimumDepositFee ? minimumDepositFee : fee;
+  function calculateDepositFee(uint256 _amount) public view returns (uint256 fee) {
+    fee = _amount * depositFee / 10_000;
+    fee = fee < minimumDepositFee ? minimumDepositFee : fee;
   }
 
-  function calculateWithdrawalFee(uint256 amount) public view returns (uint256 fee) {
-    fee = amount * withdrawalFee / 10_000;
-    fee < minimumWithdrawalFee ? minimumWithdrawalFee : fee;
+  function calculateWithdrawalFee(uint256 _amount) public view returns (uint256 fee) {
+    fee = _amount * withdrawalFee / 10_000;
+    fee = fee < minimumWithdrawalFee ? minimumWithdrawalFee : fee;
   }
 
   function updateFees(uint256 _depositFee, uint256 _withdrawalFee) external onlyOwner {
@@ -176,9 +178,9 @@ contract BeefyVaultWithdrawal {
     emit MinimumFeesUpdated(_minimumDepositFee, _minimumWithdrawalFee);
   }
 
-  function updateOwner(address newOwner) external onlyOwner {
-    owner = newOwner;
-    emit OwnerUpdated(newOwner);
+  function updateOwner(address _newOwner) external onlyOwner {
+    owner = _newOwner;
+    emit OwnerUpdated(_newOwner);
   }
 
   function withdrawFees() external {
@@ -189,10 +191,10 @@ contract BeefyVaultWithdrawal {
   }
 
   function removeMAI() external onlyOwner {
-    IERC20 mai = IERC20(MAI_ADDRESS);
-    uint256 bal = mai.balanceOf(address(this));
-    mai.transfer(msg.sender, bal);
-    emit MAIRemoved(msg.sender, bal);
+    IERC20 _mai = IERC20(MAI_ADDRESS);
+    uint256 _bal = _mai.balanceOf(address(this));
+    _mai.transfer(msg.sender, _bal);
+    emit MAIRemoved(msg.sender, _bal);
   }
 
   function updateMaxDeposit(uint256 _maxDeposit) external onlyOwner {
